8c899d3d40f961e676c669a24a8ea7c9c0c406f3
Ajay Gandecha <ajgandecha@gmail.com>
2024-12-03 17:06:07 -0500

    Fix Breadcrumbs

--- components/widgets/header.tsx
+++ components/widgets/header.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -5,27 +7,31 @@ import {
     BreadcrumbList,
     BreadcrumbPage,
     BreadcrumbSeparator,
-  } from "@/components/ui/breadcrumb"
-  
-  
+} from "@/components/ui/breadcrumb"
+import { usePathname } from 'next/navigation'
+
 export default function Header() {
-return (
-    <div className="flex h-16 items-center">
-    <Breadcrumb>
-    <BreadcrumbList>
-        <BreadcrumbItem>
-        <BreadcrumbLink href="/">COMP 426</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-        <BreadcrumbLink href="/components">Components</BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-        <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
-    </BreadcrumbList>
-    </Breadcrumb>
-    </div>
-);
+
+    const path = usePathname();
+    const pathComponents = path.split('/').splice(1).filter((s) => s !== '');
+    console.log(pathComponents);
+    return (
+        <div className="flex h-16 items-center">
+        <Breadcrumb>
+        <BreadcrumbList className="!list-none">
+            <BreadcrumbItem>
+                <BreadcrumbLink href="/">COMP 426</BreadcrumbLink>
+            </BreadcrumbItem>
+            {pathComponents.length > 0 && pathComponents.map((component, index) => (
+                <div key={index} >
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                    <BreadcrumbLink className="capitalize">{ component }</BreadcrumbLink>
+                </BreadcrumbItem>
+                </div>
+            ))}
+        </BreadcrumbList>
+        </Breadcrumb>
+        </div>
+    );
 }
